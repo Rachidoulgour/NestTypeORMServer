85261ff7084356e4a2ff95e1d5a54a366be95645
Rachidoulgour <olgor@hotmail.com>
2023-12-29 19:02:59 +0100

    user decorator added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,7 @@ import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './/auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Controller('auth')
 export class UsersController {
