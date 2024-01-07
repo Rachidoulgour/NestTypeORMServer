f9649f69c56e3d199ce0f72343beaee2c5d82a82
Rachidoulgour <olgor@hotmail.com>
2024-01-07 20:03:40 +0100

    create function added to reportsService

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
     constructor(private reportsService: ReportsService) {}
+
     @Post()
     @UseGuards(AuthGuard)
     createReport(@Body() body: CreateReportDto) {
