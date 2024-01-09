eec1571990457a46fbc83600a84b159c186f68c9
Rachidoulgour <olgor@hotmail.com>
2024-01-09 09:25:52 +0100

    admin guard created

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -3,6 +3,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/users.entity';
 import { AuthGuard } from '../guards/auth.guard';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
@@ -20,6 +21,6 @@ export class ReportsController {
 
     @Patch('/:id')
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
-        
+        return this.reportsService.changeApproval(id, body.approved);
     }
 }
